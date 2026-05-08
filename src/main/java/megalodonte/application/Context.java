b5093414c1b81331c5e6f87faa6e5f346f51c348
Eliezer o grande dev <eliezerassuncaocustodio@gmail.com>
2026-05-08 19:40:48 -0300

    feat: Execution of onMount on ScreenComponent Deprecated older useView()

--- src/main/java/megalodonte/application/Context.java
+++ src/main/java/megalodonte/application/Context.java
@@ -1,9 +1,11 @@
 package megalodonte.application;
 
+import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import megalodonte.base.components.ComponentInterface;
+import megalodonte.base.components.ScreenComponent;
 import megalodonte.base.route.RouterBase;
 
 public final class Context {
@@ -19,6 +21,28 @@ public final class Context {
         return stage;
     }
 
+    public void useView(Class<? extends  ScreenComponent> componentClass) {
+        try {
+            ScreenComponent component = componentClass.getDeclaredConstructor().newInstance();
+            useView(component);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to instantiate: " + componentClass.getName(), e);
+        }
+    }
+
+    public void useView(ScreenComponent component) {
+       var parentLayout = (Parent) component.render().getJavaFxNode();
+        stage.setScene(new Scene(parentLayout, width, height));
+
+        //após o stage.show() do Bootstrap executar. O onMount vai rodar com o stage já visível e a Scene já anexada.
+        Platform.runLater(component::onMount);
+    }
+
+    /**
+     * Advise: onMount will not work using this method
+     * @param component
+     */
+    @Deprecated(forRemoval = true)
     public void useView(ComponentInterface<?> component) {
         stage.setScene(new Scene((Parent) component.getJavaFxNode(), width, height));
     }
