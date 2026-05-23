26de825526f8afe9204fc7812f07b2caee1a970f
eliezer-super-dev <eliezerassuncaocustodio@hotmail.com>
2026-05-23 17:27:56 -0300

    fix: Redirect.to() now runs on separated thread

--- src/main/java/megalodonte/base/Redirect.java
+++ src/main/java/megalodonte/base/Redirect.java
@@ -1,14 +1,27 @@
 package megalodonte.base;
 import java.awt.Desktop;
 import java.net.URI;
+import java.util.function.Consumer;
 
 final public class Redirect {
 
     public static void to(String url){
-        try {
-            Desktop.getDesktop().browse(new URI(url));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        new Thread(()->{
+            try {
+                Desktop.getDesktop().browse(new URI(url));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public static void to(String url, Consumer<Exception> exceptionCallback){
+        new Thread(()->{
+            try {
+                Desktop.getDesktop().browse(new URI(url));
+            } catch (Exception e) {
+                exceptionCallback.accept(e);
+            }
+        }).start();
     }
 }
