969aef2d2f5cfa87c724f5eaa689114478566666
eliezer-software-enginner <93846923+eliezer-dev-software-enginner@users.noreply.github.com>
2026-07-11 19:01:52 -0300

    feat: moved Props to this package from megalodonte-components

---
+++ src/main/java/megalodonte/base/components/Props.java
@@ -0,0 +1,28 @@
+package megalodonte.base.components;
+
+import javafx.scene.Node;
+import megalodonte.base.state.ReadableState;
+import megalodonte.base.theme.ThemeInterface;
+import megalodonte.base.theme.ThemeManager;
+
+import java.util.function.Consumer;
+
+public abstract class Props implements PropsInterface {
+
+    final public void apply(Node node) {
+        bindStates(node);
+
+        ThemeManager.state().subscribe(theme -> {
+            if (theme == null) return;
+            applyTheme(node, this, theme);
+        });
+    }
+
+    protected void bindStates(Node node) {}
+
+    protected <T> void bind(Node node, ReadableState<T> state, Consumer<T> handler) {
+        if (state != null) state.subscribe(handler);
+    }
+
+    protected abstract void applyTheme(Node node, Props props, ThemeInterface theme);
+}
